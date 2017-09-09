24c9b60982dd06aa65f196dca3aae1b446f3512c
Vincent Lee <tienchil@hotmail.com>
2017-09-09 19:37:52 -0400

    tuning

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,7 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
-  pid.Init(0.05, 0.0, 0.06);
+  pid.Init(0.05, 0.0, 0.07);
 
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
