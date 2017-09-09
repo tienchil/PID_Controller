0bcb493721c5e5a336733d09c06864000882ded7
Vincent Lee <tienchil@hotmail.com>
2017-09-09 17:25:51 -0400

    Finish the controller

---
+++ src/PID.cpp
@@ -0,0 +1,31 @@
+#include "PID.h"
+
+using namespace std;
+
+/*
+* TODO: Complete the PID class.
+*/
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kp, double Ki, double Kd) {
+	PID::Kp = Kp;
+	PID::Ki = Ki;
+	PID::Kd = Kd;
+	p_error = 0.0;
+	i_error = 0.0;
+	d_error = 0.0;
+}
+
+void PID::UpdateError(double cte) {
+	p_error = cte;
+	i_error += cte;
+	d_error = cte - d_error;
+}
+
+double PID::TotalError() {
+	return p_error + i_error + d_error;
+}
+
